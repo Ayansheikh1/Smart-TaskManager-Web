a9842fd953b1e538c4f5d772012573afe8bb3a23
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-04 17:13:13 +0530

    add user model with schema for username, email, and password

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"username already taken"],
+        min:3,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:[true,"email already taken"],
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        min:8,
+        required:true
+    },
+   
+});
+
+const userModel = mongoose.model('users',userSchema);
+
+module.exports = userModel;
