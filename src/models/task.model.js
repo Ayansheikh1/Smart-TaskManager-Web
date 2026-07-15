7bd64c2eb6f6a4125b8a84e44c13577144ff4593
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-15 19:01:37 +0530

    feat: create task model with schema for managing tasks

---
+++ src/models/task.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema({
+    title:{
+        type:String,
+        trim:true,
+        minlength:[3,"title must be at least 3 characters long"],
+        maxlength:[100,"title cannot exceed 100 characters"],
+        required:[true,"title is necessary"]
+    },
+    description:{
+         type:String,
+         trim:true,
+    },
+    status:{
+        type:String,
+        default:"Todo",
+        enum:{
+            values:["Todo","In Progress","Completed"],
+            message:"status should be either Todo,In Progress or Completed"
+        },
+        
+    },
+    priority:{
+        type:String,
+        default:"Medium",
+        enum:{
+            values:["Low","Medium","High"],
+            message:"priority should be either Low,Medium or High"
+        },
+       
+    },
+    dueDate:{
+        type:Date,
+        
+
+        
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:[true,"owner is necessary"]
+    }
+},{
+    timestamps:true
+})
+
+const taskModel = mongoose.model("tasks",taskSchema);
+
+module.exports = taskModel;
+    
