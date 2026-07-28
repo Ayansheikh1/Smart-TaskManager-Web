cbffabbf28359a9afffde4401ef8c462660d3cdf
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-28 21:07:06 +0530

    feat: add useAuth hook for user authentication and registration handling

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import { AuthContext } from "../context/auth.context"
+import { registerUser } from "../services/authApi";
+
+
+export const useAuth = () =>{
+    const context = useContext(AuthContext);
+    const{user,setUser,loading,setLoading} = context;
+
+    const handleRegister = async({username,email,password}) =>{
+        setLoading(true);
+        try {
+            const data = await registerUser({username,email,password});
+            setUser(data.user);
+        } catch (error) {
+            console.log(error)
+        }finally {
+            setLoading(false);
+        }
+    }
+}
