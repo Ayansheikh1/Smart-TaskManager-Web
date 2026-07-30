ef677d8dbef619d17d566a264e4e305cddb402da
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-30 19:21:57 +0530

    feat: add login functionality and update register component

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { AuthContext } from "../context/auth.context"
-import { registerUser } from "../services/authApi";
+import { loginUser, registerUser } from "../services/authApi";
 
 
 export const useAuth = () =>{
@@ -18,6 +18,23 @@ export const useAuth = () =>{
             setLoading(false);
         }
     }
+
+
+
+    const handleLogin = async({email,password}) =>{
+        setLoading(true);
+        try{
+            const data = await loginUser({email,password});
+            setUser(data.user);
+
+        }catch(error){
+            console.log(error)
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    
 }
 
 
