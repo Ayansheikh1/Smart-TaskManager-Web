050bab48fcc45757f1dd6d14c1f351c2ac2ab4d7
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-30 19:34:29 +0530

    feat: enhance Register component layout and structure

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -5,7 +5,26 @@ const Register = () => {
 
   return (
    <main>
-   
+  <div className="form-container">
+    <h1>Register</h1>
+  <form>
+    <div className="input-group">
+        <label htmlFor="username">Username</label>
+        <input type='text' name='username' id='username' placeholder='Enter username'/>
+    </div>
+    <div className="input-group">
+        <label htmlFor="email">Email</label>
+        <input type='email' name='email' id='email' placeholder='Enter email'/>
+    </div>
+    <div className="input-group">
+        <label htmlFor="username">Username</label>
+        <input type='password' name='password' id='password' placeholder='Enter password'/>
+    </div>
+
+    <button className='button'>Register</button>
+  </form>
+
+  </div>
    </main>
   )
 }
