8b9be7adbe174482d93ae8100cb6fa4255573f3f
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-08-01 00:25:33 +0530

    feat: integrate Tailwind CSS and enhance authentication flow
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated package.json to include Tailwind CSS dependencies.
    - Refactored Login and Register components to utilize useAuth hook for authentication.
    - Implemented loading state in Login and Register components during authentication.
    - Added form handling for email and password inputs in Login and Register components.
    - Imported Tailwind CSS styles in main.jsx and created a new style.css file.

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../hooks/useAuth'
+import { useNavigate } from 'react-router';
 
 const Register = () => {
 
 
+    const navigate = useNavigate();
+    const [username,setUsername] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const {loading,register} = useAuth();
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        await register({username,email,password});
+        navigate('/')
+    }
+
+    if(loading){
+                return(
+                    
+        
+                    <h1>
+                        Loading.......
+                    </h1>
+                )
+            }
 
 
   return (
    <main>
   <div className="form-container">
     <h1>Register</h1>
-  <form>
+  <form onSubmit={handleSubmit}>
     <div className="input-group">
         <label htmlFor="username">Username</label>
-        <input type='text' name='username' id='username' placeholder='Enter username'/>
+        <input 
+        onChange={(e)=>{setUsername(e.target.value)}}
+        type='text' name='username' id='username' placeholder='Enter username'/>
     </div>
     <div className="input-group">
         <label htmlFor="email">Email</label>
-        <input type='email' name='email' id='email' placeholder='Enter email'/>
+        <input 
+        onChange={(e)=>{setEmail(e.target.value)}}
+        type='email' name='email' id='email' placeholder='Enter email'/>
     </div>
     <div className="input-group">
         <label htmlFor="username">Password</label>
-        <input type='password' name='password' id='password' placeholder='Enter password'/>
+        <input 
+        onChange={(e)=>{setPassword(e.target.value)}}
+        type='password' name='password' id='password' placeholder='Enter password'/>
     </div>
 
     <button className='button'>Register</button>
