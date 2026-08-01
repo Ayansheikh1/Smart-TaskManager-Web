94ae433b29d9d8708c9085e4de8ded7e1333e7fb
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-08-01 15:55:07 +0530

    feat: add CORS middleware to backend and configure for localhost
    - Added `cors` package to `package.json`.
    - Integrated CORS middleware in `app.js` to allow requests from `http://localhost:5173` with credentials.
    - Updated `AuthProvider` in `auth.context.jsx` to set loading state to false initially.
    - Modified `useAuth` hook to return an object instead of an array for better destructuring.
    - Added necessary files for `cors` and `object-assign` packages including licenses and readme.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const authRouter = require('./routes/auth.routes')
 const taskRouter = require('./routes/task.routes')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express();
 
 
 app.use(express.json());//middleware
 app.use(cookieParser());
+app.use(cors({
+     origin:'http://localhost:5173',
+    credentials:true
+}))
 
 
 app.use("/api/auth",authRouter);
