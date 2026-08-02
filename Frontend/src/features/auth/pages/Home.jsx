6e2965315f3b09282750999e43b921aac408cce0
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-08-02 15:48:10 +0530

    feat: display username and add logout functionality in Home component

--- Frontend/src/features/auth/pages/Home.jsx
+++ Frontend/src/features/auth/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useAuth } from '../hooks/useAuth'
 
 const Home = () => {
+
+  const {logout,user} = useAuth();
+  const handleLogout = async (e) =>{
+    e.preventDefault();
+    await logout();
+
+  }
+
   return (
     <main>
-        <h1>Welcome</h1>
+        <h1>Welcome {user?.username}</h1>
+        <button onClick={handleLogout} >Logout</button>
         
     </main>
   )
