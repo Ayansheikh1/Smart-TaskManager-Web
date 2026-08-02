3e7c46282de96be7f5e3b9136ce582593b207baf
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-08-02 15:11:03 +0530

    feat: wrap Home component with Protected for route protection fix: set initial loading state to false in AuthProvider

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from 'react-router'
 import Register from '../src/features/auth/pages/Register'
 import Login from './features/auth/pages/Login'
 import Home from './features/auth/pages/Home'
+import Protected from './features/auth/components/Protected'
 
 
 export const router = createBrowserRouter([
@@ -17,6 +18,6 @@ export const router = createBrowserRouter([
     },
     {
         path:"/",
-        element:<Home/>
+        element: <Protected> <Home/> </Protected> 
     }
 ])
