bad9512dea58e5a34bfba1a12037470817664857
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-08-12 21:51:11 +0530

    feat: implement task API service with CRUD operations for task management

---
+++ Frontend/src/features/task/services/taskApi.js
@@ -0,0 +1,31 @@
+import api from "../../../../shared/api/axios";
+
+
+export async function createTask(taskData){
+   const response = await api.post('/api/task/create',taskData);
+   return response.data
+}
+
+export async function getAllTasks(){
+   const response = await api.get('/api/task/tasks');
+   return response.data
+}
+
+
+export async function getTaskById(taskId){
+   const response = await api.get(`/api/task/tasks/${taskId}`,);
+   return response.data
+}
+
+
+export async function updateTask(taskId,taskData){
+   const response = await api.patch(`/api/task/tasks/update/${taskId}`,taskData);
+   return response.data
+}
+
+
+export async function deleteTask(taskId){
+   const response = await api.delete(`/api/task/tasks/delete/${taskId}`);
+   return response.data
+}
+
