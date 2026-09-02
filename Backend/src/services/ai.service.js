e08991618ec1ac47fda7effa51a82695d5e7a69e
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-09-02 18:34:01 +0530

    feat: enhance task generation logic with schema validation and AI integration

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,12 +1,37 @@
 const { GoogleGenAI } = require('@google/genai');
+const { z } = require('zod')
+
 
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
 
 
 
-const taskSchema = 
+const taskSchema = z.object({
+    title: z.string().min(3).max(100).describe("Title of the task"),
+    description:z.string().describe("Description of the task"),
+    priority:z.enum(["Low", "Medium", "High"]).describe("Priority of the task"),
+    status:z.enum(["Todo"]).describe("Status of the task"),
+    dueDate: z.string().describe("Due date of the task in YYYY-MM-DD format")
+})
 
 
-async function generateTask({goal}){
+async function generateTasks({goal}){
+        const prompt = `Generate a task based on this goal : ${goal}`
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents:prompt,
+            config: {
+           
+            responseMimeType: "application/json",
+            responseSchema: z.toJSONSchema(taskSchema),
+           
+        }
+        })
 
+        const task = taskSchema.parse(JSON.parse(response.text));
+        return task;
 }
+
+
+
+module.exports = { generateTasks }
