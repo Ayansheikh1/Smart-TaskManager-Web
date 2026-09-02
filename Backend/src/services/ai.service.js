10329c430a7119237d4bf693e968fcf857d257a3
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-09-02 17:44:46 +0530

    install zod

---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,12 @@
+const { GoogleGenAI } = require('@google/genai');
+
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
+
+
+
+const taskSchema = 
+
+
+async function generateTask({goal}){
+
+}
