21a63484b3b200e5f9125b1c784569a1b12a753a
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-09-03 19:07:17 +0530

    refactor: clean up whitespace and formatting in ai.service.js for improved readability

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,45 +1,35 @@
+
 const { GoogleGenAI } = require('@google/genai');
 const { z } = require('zod')
 
-
-
-
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
-
-
 
 const taskSchema = z.object({
     title: z.string().min(3).max(100).describe("Title of the task"),
-    description:z.string().describe("Description of the task"),
-    priority:z.enum(["Low", "Medium", "High"]).describe("Priority of the task"),
-    status:z.enum(["Todo"]).describe("Status of the task"),
+    description: z.string().describe("Description of the task"),
+    priority: z.enum(["Low", "Medium", "High"]).describe("Priority of the task"),
+    status: z.enum(["Todo"]).describe("Status of the task"),
     dueDate: z.string().describe("Due date of the task in YYYY-MM-DD format")
 })
 
-
-async function generateTasks({goal}){
-        const prompt = `Generate a task based on this goal : ${goal}`
-        const response = await ai.models.generateContent({
-            model: "gemini-3.6-flash",
-            contents:prompt,
-            config: {
-           
+async function generateTasks({ goal }) {
+    const prompt = `Generate a task based on this goal : ${goal}`
+    const response = await ai.models.generateContent({
+        model: "gemini-3.6-flash",
+        contents: prompt,
+        config: {
             responseMimeType: "application/json",
             responseSchema: z.toJSONSchema(taskSchema),
-           
         }
-        })
-        
+    })
 
-        const task = taskSchema.parse(JSON.parse(response.text));
-        return task;
+    console.log("Raw Gemini output:", response.text);
+
+    const task = taskSchema.parse(JSON.parse(response.text));
+    return task;
 }
 
-
-
 module.exports = { generateTasks }
-
 
 
 
