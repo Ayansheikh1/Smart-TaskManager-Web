46625d1c0336eac9d4a81400e11c9b1c69a3e314
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-09-13 20:34:49 +0530

    Update server.js to listen on environment-defined port

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,8 @@ console.log('Key loaded:', process.env.GEMINI_API_KEY ? 'yes' : 'no');
 connectToDB()
 
 
-app.listen(3000,()=>{
-console.log('server is running on port 3000');
+app.listen(process.env.PORT,()=>{
+console.log('server is running ');
 })
 
 
